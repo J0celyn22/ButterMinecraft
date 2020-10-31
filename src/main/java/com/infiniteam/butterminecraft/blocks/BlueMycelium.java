64f3aef638d2dd2a5176fee8fb2aaf19eb96c870
J0celyn <jocelyn.girault@orange.fr>
2020-10-31 19:23:44 +0100

    Adding the correct texture for the mushroom blocks

--- src/main/java/com/infiniteam/butterminecraft/blocks/BlueMycelium.java
+++ src/main/java/com/infiniteam/butterminecraft/blocks/BlueMycelium.java
@@ -1,19 +1,23 @@
 package com.infiniteam.butterminecraft.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.GrassBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraftforge.common.ToolType;
 
-public class BlueMycelium extends Block {
+import java.util.function.ToIntFunction;
+
+public class BlueMycelium extends GrassBlock {
 
     public BlueMycelium() {
         super(Properties.create(Material.EARTH)
                 .hardnessAndResistance(1.0f, 1.0f)
                 .harvestLevel(1)
                 .harvestTool(ToolType.SHOVEL)
-                /*.setRequiresTool()*/
-                //.lightLevel((state) -> 6))
-                .variableOpacity()
+                //.setRequiresTool()
+                //.variableOpacity()
         );
     }
+
 }
